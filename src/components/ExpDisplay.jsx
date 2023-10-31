60c3b09026e26c90ebc00c87326fe9b457a658b9
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2023-10-31 14:16:57 +0900

    Add ExpDisplay component

--- src/components/ExpDisplay.jsx
+++ src/components/ExpDisplay.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
-const ExpDisplay = () => {
-  return <div>ExpDisplay</div>;
+import "../styles/ExpDisplay.scss";
+const ExpDisplay = ({ experienceInfo }) => {
+  return (
+    <div className="exp-display">
+      <h3>Experience</h3>
+      <div className="expInfo-list">
+        <div className="left">
+          <div className="date-groups">
+            <span>{experienceInfo.startDate}</span> ~
+            <span> {experienceInfo.startDate}</span>
+          </div>
+          <p className="location">{experienceInfo.location}</p>
+        </div>
+        <div className="right">
+          <p className="company">{experienceInfo.company}</p>
+          <p className="position">{experienceInfo.position}</p>
+          <p className="description">{experienceInfo.description}</p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpDisplay;
